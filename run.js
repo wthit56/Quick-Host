69e922f6bea277d205246ed0a475ec6045acca50
Thomas Giles <wthit56@googlemail.com>
2014-08-03 17:32:39 +0100

    redone to list directory files

--- run.js
+++ run.js
@@ -64,119 +64,128 @@ function initHost(port) {
 	http.createServer(function(request, response) {
 		console.log(request.url);
 
-		var filepath = decodeURI(url.parse(request.url).pathname);
-		filepath = path.join(path.join(args[2], filepath));
-		if (filepath[filepath.length - 1] === "\\") {
-			filepath += "index.html";
-		}
-		
 		var callback = CreateCallback();
-		callback.filepath = filepath;
+		
+		callback.path = url.parse(request.url).pathname;
+		callback.filepath = path.join(path.join(args[2], decodeURI(callback.path)));
+		callback.tryFile = callback.filepath;
+		callback.index = false;
+		
 		callback.request = request;
 		callback.response = response;
 		callback.onDispose = function(callback) {
+			delete callback.index;
 			delete callback.filepath;
 			delete callback.request;
 			delete callback.response;
 		};
 
-		fs.exists(filepath, callback.setAction(exists));
+		fs.exists(callback.tryFile, callback.setAction(exists));
 	}).listen(port);
 	
-	function _404(callback) {
-		console.log("404 " + callback.request.url + " : " + callback.filepath);
+	function render_404(callback) {
+		console.log("404 " + callback.request.url + " : " + callback.tryFile);
 		callback.response.writeHead(404, { "Content-Type": "text/html" });
-		callback.response.write("<title>404</title><h1>404: File not found</h1>")
+		callback.response.write("<title>404</title><h1>404: Not Found</h1>")
 		callback.response.end();
-		//callback.response.end();
-		
 		callback.dispose();
 	}
-
-	function exists(exists, callback) {
-		if (!exists) {
-			_404(callback);
-		}
-		else {
-			fs.stat(callback.filepath, callback.setAction(stat));
-		}
-	}
-
-	function stat(error, stats, callback) {
-		if(stats.isFile()) {
-			var ext = path.extname(callback.filepath);
-			if (ext && (ext in mime)) {
-				ext = mime[ext];
-				callback.response.writeHead(200, { "Content-Type": ext });
-			}
-			else {
-				callback.response.writeHead(200);
-			}
-
-			fs.createReadStream(callback.filepath).on("end", callback.dispose).pipe(callback.response);
-			console.log("200 " + (ext ? "[" + ext + "] " : "") + callback.request.url + " : " + callback.filepath);
-		}
-		else if(stats.isDirectory()) {
-			console.log("301 premanent redirect to " + callback.request.url + "/");
-			callback.response.writeHead(301, {location:callback.request.url+"/"});
-			callback.response.end();
-			callback.dispose();
-			//callback.filepath = path.join(callback.filepath, "index.html");
-			//fs.exists(callback.filepath, callback.setAction(exists));
-		}
-	}
-
-	/*
-	http.createServer(function (request, response) {
-		console.log(request.url);
-
-		var filepath = decodeURI(url.parse(request.url).pathname);
-		filepath = path.join(path.join(args[2], filepath));
-		if (filepath[filepath.length - 1] === "\\") {
-			filepath += "index.html";
-		}
-
-		fs.exists(filepath, function (e) {
-			exists(request, response, filepath, e);
-		});
-	}).listen(port);
 	
-	function _404(request, response, filepath) {
-		console.log("404 " + request.url + " : " + filepath);
-		response.writeHead(404, { "Content-Type": "text/html" });
-		response.write("<title>404</title><h1>404</h1>");
-		response.end();		
-	}
-	function exists(request, response, filepath, exists) {
-		if (!exists) {
-			_404(request, response, filepath);
+	var render_dir = (function() {
+		function render_dir(callback){
+			fs.readdir(callback.filepath, callback.setAction(readdir));
 		}
-		else {
-			fs.stat(filepath, function(error, s){
-				stat(request, response, filepath, error, s);
-			});
-		}
-	}
-	function stat(request, response, filepath, error, stats) {
-		if(stats.isFile()) {
-			var ext = path.extname(filepath);
-			if (ext && (ext in mime)) {
-				ext = mime[ext];
-				response.writeHead(200, { "Content-Type": ext });
+		
+		function readdir(error, files, callback) {
+			console.log(error, files);
+			
+			if (error) {
+				console.log(callback.tryFile + ": ERROR: " + error);
+				render_404(callback);
 			}
 			else {
-				response.writeHead(200);
+				console.log(files);
+				console.log(
+					"200 (directory listing) [text/html] " +
+					callback.request.url + " : " + callback.filepath
+				);
+				var response = callback.response;
+				response.writeHead(200, {"Content-type": "text/html" });
+				response.write(
+					"<title>" + callback.path + "</title>" +
+					"<h1>"+callback.path+"</h1>"
+				);
+				response.write(
+					(files.length>0)
+						? "<ul>"+files.map(linkFiles, callback).join("")+"</ul>"
+						: "<p>No files found</p>"
+				);
+				response.end();
+				
+				callback.dispose();
 			}
-
-			fs.createReadStream(filepath).pipe(response);
-			console.log("200 " + (ext ? "[" + ext + "] " : "") + request.url + " : " + filepath);
 		}
-		else if(stats.isDirectory()) {
-			filepath = path.join(filepath, "index.html");
-			fs.exists(filepath, function (e) {
-				exists(request, response, filepath, e);
-			});
+		function linkFiles(file) {
+			return "<li><a href=\""+path.join(this.path,file)+"\">"+file+"</a></li>";
+		}
+		
+		return render_dir;
+	})();
+	
+	function exists(exists, callback) {
+		if (exists) {
+			fs.stat(callback.tryFile, callback.setAction(stat));
+		}
+		else if (callback.index) {
+			render_dir(callback);
+		}
+		else {
+			console.log("404 " + callback.request.url + " : " + callback.filepath);
+			render_404(callback);
 		}
 	}
-	*/
+	function stat(error, stat, callback) {
+		if(error){
+			console.log(callback.tryFile + ": ERROR: " + error);
+		}
+		else {
+			if(stat.isFile()){
+				var ext = path.extname(callback.tryFile);
+				if (ext && (ext in mime)) {
+					ext = mime[ext];
+					callback.response.writeHead(200, { "Content-Type": ext });
+				}
+				else {
+					callback.response.writeHead(200);
+				}
+
+				fs.createReadStream(callback.tryFile)
+					.on("end", callback.dispose)
+					.pipe(callback.response);
+				
+				console.log(
+					"200 " + (ext ? "[" + ext + "] " : "") +
+					callback.request.url + " : " + callback.filepath
+				);
+			}
+			else if (stat.isDirectory() && !callback.index) {
+				if(callback.path[callback.path.length-1]!=="/"){
+					console.log("301 premanent redirect to " + callback.request.url + "/");
+					callback.response.writeHead(301, {location:callback.request.url+"/"});
+					callback.response.end();
+					callback.dispose();
+				}
+				else {
+					callback.index = true;
+					fs.exists(
+						callback.tryFile = path.join(callback.filepath, "index.html"),
+						callback.setAction(exists)
+					);
+				}
+			}
+			else {
+				render_404(callback);
+			}
+		}
+	}
 }
