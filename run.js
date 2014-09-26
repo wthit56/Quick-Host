7b0da84c8b8ea08e0b60dd7ab05001837160e816
Thomas Giles <wthit56@googlemail.com>
2014-09-26 13:02:21 +0100

    run.js formatting

--- run.js
+++ run.js
@@ -4,8 +4,8 @@ if (args.length < 3) { throw "Must specify folder."; }
 var QuickHost = require("Quick-Host");
 
 QuickHost(args[2], args[3], function(error, port) {
-	if(error){throw error;}
-	else{
+	if (error) { throw error; }
+	else {
 		console.log("Quick-Hosting \"" + args[2] + "\" using port " + port + "... (CTRL+C or close to stop)");
 	}
 });
